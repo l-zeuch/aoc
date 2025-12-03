0dbbc4b3eb769637d81af581c458120425c3b38c
Luca Zeuch <l-zeuch@email.de>
2025-12-03 13:55:24 +0100

    day3: some cleanup and commentary
    Signed-off-by: Luca Zeuch <l-zeuch@email.de>

--- 2025/day3/day3.py
+++ 2025/day3/day3.py
@@ -25,10 +25,8 @@ def part2(lines):
     total_jolts = 0
 
     for bank in lines:
-        batteries = [int(x) for x in bank]
-
+        search_bank = [int(x) for x in bank]
         results = []
-        search_bank = list(batteries)
 
         for i in range(12):
             if len(search_bank) == 0:
@@ -37,17 +35,19 @@ def part2(lines):
 
             largest = -1
             index_of_largest = 0
-
-            need = 12-i
+            need = 12-i # we take one battery from each iteration
             can_take = len(search_bank) - need
 
+            # construct a search window that ranges from the start (or the previously found max)
+            # to however many we can still take: [max, can_take]
+            # this means we're not prematurely exhausting our bank.
+            # the search window is not always set to how many we still need!
             for idx, v in enumerate(search_bank[:can_take+1]):
                 if v > largest:
                     largest = v
                     index_of_largest = idx
-
-
             results.append(largest)
+            # slice it up: we want to start at the found max number in the next iteration
             search_bank = search_bank[(index_of_largest + 1):]
 
         tmp = 0
@@ -61,9 +61,9 @@ def main():
     with open("input.txt", "rt") as fin:
         lines = [line.strip() for line in fin]
     lines_lst = list(lines) # create an immutable reference
-    #sol1 = part1(lines_lst)
+    sol1 = part1(lines_lst)
     sol2 = part2(lines_lst)
-    print(f"part 1: placeholder; part 2: {sol2}")
+    print(f"part 1: {sol1}; part 2: {sol2}")
 
 if __name__ == "__main__":
     main()
